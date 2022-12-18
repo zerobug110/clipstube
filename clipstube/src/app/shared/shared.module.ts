2b28366559eaab382d721fb6ac0c652c8d0419f9
zerobug110 <mahabojang110@gmail.com>
2022-12-18 00:48:19 +0000

    structured tab modal files

--- clipstube/src/app/shared/shared.module.ts
+++ clipstube/src/app/shared/shared.module.ts
@@ -2,18 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ModalComponent } from './modal/modal.component';
 import { ModalService } from '../services/modal.service';
+import { TabsContainerComponent } from './tabs-container/tabs-container.component';
+import { TabsComponent } from './tabs/tabs.component';
 
 
 
 @NgModule({
   declarations: [
-    ModalComponent
+    ModalComponent,
+    TabsContainerComponent,
+    TabsComponent
   ],
   imports: [
     CommonModule,
   ],
   exports: [
-    ModalComponent
+    ModalComponent,
+    TabsComponent,
+    TabsContainerComponent
   ],
 })
 export class SharedModule { }
